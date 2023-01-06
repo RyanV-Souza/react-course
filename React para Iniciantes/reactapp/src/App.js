5be4fe9a8e904480564c19f5d2d19eec7822c2d7
Ryan Vieira Souza <dev18.gbmlogistica@outlook.com>
2023-01-06 08:52:17 -0300

    JSX2 Exercicio

--- React para Iniciantes/reactapp/src/App.js
+++ React para Iniciantes/reactapp/src/App.js
@@ -1,5 +1,58 @@
-function App() {
-  return <div>Meu App</div>;
-}
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
+
+const App = () => {
+  const dados = mario;
+  const total = dados.compras.reduce(
+    (a, b) => a + Number(b.preco.split(" ")[1]),
+    0
+  );
+
+  return (
+    <div>
+      <p>
+        <strong>Nome: </strong> {dados.cliente}
+      </p>
+      <p>
+        <strong>Idade: </strong> {dados.idade}
+      </p>
+      <p>
+        <strong>Situação: </strong>{" "}
+        <span style={{ color: dados.ativa ? "green" : "red" }}>
+          {dados.ativa ? "Ativa" : "Inativa"}
+        </span>
+      </p>
+      <p>
+        <strong> Total gasto: </strong> R$ {total}
+      </p>
+
+      {total > 10000 && <p> Você está gastando demais</p>}
+    </div>
+  );
+};
 
 export default App;
