6a51e00f8d70a1a640f84f2460a41e9a78c7215b
Ryan Vieira Souza <dev18.gbmlogistica@outlook.com>
2023-01-09 19:34:54 -0300

    Custom Hooks

--- React para Iniciantes/reactapp/src/App.js
+++ React para Iniciantes/reactapp/src/App.js
@@ -1,34 +1,12 @@
-import { useState, useEffect } from "react";
-import Product from "./Product";
+import React from "react";
+import useLocalStorage from "./useLocalStorage";
+import useFetch from "./useFetch";
 
 const App = () => {
-  const [product, setProduct] = useState(null);
-
-  async function handleClick(event) {
-    setProduct(event.target.innerText);
-  }
-
-  useEffect(() => {
-    const productLocalStorage = window.localStorage.getItem("produto");
-
-    if (productLocalStorage !== null) setProduct(productLocalStorage);
-  }, []);
-
-  useEffect(() => {
-    if (product !== null) window.localStorage.setItem("produto", product);
-  }, [product]);
-
   return (
     <>
-      <h1>Preferência: {product}</h1>
-      <button style={{ marginLeft: "3px" }} onClick={handleClick}>
-        notebook
-      </button>
-      <button style={{ marginLeft: "3px" }} onClick={handleClick}>
-        smartphone
-      </button>
-
-      {product && <Product product={product} />}
+      <button>notebook</button>
+      <button>smartphone</button>
     </>
   );
 };
